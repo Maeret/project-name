3ee6ea037e05bc548d755db74d2226df7415aef9
Maeret <margaritabolsh@gmail.com>
2024-09-28 14:39:19 +0300

    imperted all functions, testing code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { getPublicAddressByWallet, createWallet } from 'tonutils-ts';
+import { createWallet, 
+         getBouncableAddress,
+         getPublicAddressByMnenomic, 
+         getNonBouncableAddress,
+         getPublicAddressByWallet } from 'tonutils-ts';
+import { Address } from '@ton/core';
 
 @Injectable()
 export class AppService {
   async getAddress(): Promise<string> {
-    
     const { generatedWallet } = await createWallet();
 
-    const address = getPublicAddressByWallet(generatedWallet);
-    return address.toString();
+    const addressString = generatedWallet.address.toString( {testOnly: true});
+
+    if (Address.isAddress(addressString)) {
+      console.log('The address is valid:', addressString);
+      return addressString;
+    } else {
+      console.log('The address is not valid:', addressString);
+      throw new Error('Invalid wallet address.');
+    }
   }
 }
